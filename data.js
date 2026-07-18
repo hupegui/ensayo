9f1e30199f91c740ec9e00a5f9150a3908c2391e
Hector Upegui <73083305+hupegui@users.noreply.github.com>
2026-07-19 00:49:06 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+window.projectData = [
+  {
+    "id": "node-I001-0",
+    "title": "Bienvenido",
+    "type": "folder",
+    "children": []
+  },
+  {
+    "id": "node-I003-1",
+    "title": "Encuesta",
+    "type": "folder",
+    "children": []
+  },
+  {
+    "id": "node-I005-2",
+    "title": "Material",
+    "type": "folder",
+    "children": [
+      {
+        "id": "node-I00C-0",
+        "title": "Nuevo Enlace",
+        "type": "external",
+        "url": "www.semana.co"
+      },
+      {
+        "id": "node-I00D-1",
+        "title": "Bonus",
+        "type": "external",
+        "url": "\"C:\\Users\\upega\\Mis_proyectos_python\\HTML_to_SPA\\Bonus-personalisierter Agent.pdf\""
+      },
+      {
+        "id": "node-I00E-2",
+        "title": "Nuevo Documento",
+        "type": "direct",
+        "content": "<h1>Nuevo Contenido</h1>"
+      },
+      {
+        "id": "node-I00F-3",
+        "title": "Ensayo pdf",
+        "type": "external",
+        "url": "C:\\Users\\upega\\Mis_proyectos_python\\HTML_to_SPA\\Bonus-personalisierter Agent.pdf"
+      }
+    ]
+  },
+  {
+    "id": "node-I007-3",
+    "title": "Acerca de Nosotros",
+    "type": "folder",
+    "children": [
+      {
+        "id": "node-I009-0",
+        "title": "Dr. Hector Upegui",
+        "type": "direct",
+        "content": "<div class=\"max-w-4xl mx-auto px-4 py-10\">\n    <h2 class=\"text-3xl font-bold mb-10 text-center text-gray-800\">Über mich</h2>\n    <div style=\"display: flex; flex-direction: column; align-items: center; justify-content: center; width: 100%;\">\n        <div style=\"margin: 0 auto; display: inline-block; min-width: 250px;\">\n            <div class=\"badge-base LI-profile-badge\" data-locale=\"de_DE\" data-size=\"large\" data-theme=\"light\" data-type=\"VERTICAL\" data-vanity=\"hectorupegui\" data-version=\"v1\">\n                <a class=\"badge-base__link LI-simple-link\" href=\"https://de.linkedin.com/in/hectorupegui?trk=profile-badge\"></a>\n            </div>\n        </div>\n        <div class=\"mt-8\">\n            <a href=\"https://www.linkedin.com/in/hectorupegui\" target=\"_blank\" class=\"inline-flex items-center gap-3 linkedin-btn rounded-full font-bold transition-all shadow-xl\">\n                <svg class=\"w-6 h-6\" fill=\"currentColor\" viewBox=\"0 0 24 24\">\n                    <path d=\"M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z\" />\n                </svg>\n                Mein LinkedIn Profil besuchen\n            </a>\n        </div> \n    </div>\n</div>"
+      },
+      {
+        "id": "node-I010-1",
+        "title": "biatris",
+        "type": "folder",
+        "children": []
+      }
+    ]
+  }
+];
